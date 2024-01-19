a4579d053418cf74bc1d0e252f119761d48fd8b8
soumayadip2002 <soumayadipsaha2002@gmail.com>
2024-01-19 23:18:51 +0530

    updated profile ✅🚀

--- frontend/teacher/src/pages/Profile.jsx
+++ frontend/teacher/src/pages/Profile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Helmet } from "react-helmet-async";
 const Profile = () => {
-  return <div>profile</div>;
+  return(
+    <div className="w-[98%] m-auto px-3 mt-4 md:mt-[4rem]">
+      <Helmet>
+        <title>Profile</title>
+      </Helmet>
+      courses
+    </div>
+  );
 };
 
 export default Profile;
