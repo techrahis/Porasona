12f1ed6911056565c7659028404bbdce0d9ccb95
Soumili Mondal <soumili.mondal65@gmail.com>
2024-01-20 20:07:34 +0530

    earning card ui

--- frontend/teacher/src/pages/Dashboard.jsx
+++ frontend/teacher/src/pages/Dashboard.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { Helmet } from "react-helmet-async";
-import TotalEarnings from "@/components/shared/TotalEarnings";
+
 const Dashboard = () => {
   return (
     <div>
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <TotalEarnings />
+      
     </div>
   );
 };
